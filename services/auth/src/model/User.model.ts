c3caf5fbf9a9c561b6e415c67b025cba805dfccd
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2026-02-12 01:35:28 +0530

    feat(auth): initialize auth service with MongoDB connection and user model
    - Added package.json for auth service with dependencies and scripts.
    - Implemented MongoDB connection in db.ts.
    - Created index.ts to set up Express server.
    - Defined User model with Mongoose in User.model.ts.
    - Configured TypeScript settings in tsconfig.json.

---
+++ services/auth/src/model/User.model.ts
@@ -0,0 +1,21 @@
+import mongoose,{Schema,Document} from "mongoose";
+
+export interface IUser extends Document {
+name: string;
+email:string;
+image:string;   
+role:string;
+}
+
+const schema = new Schema<IUser>({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    image:{type:String,required:true},
+    role:{type:String,default:"user"}
+},{
+    timestamps:true
+});
+const User = mongoose.model<IUser>('User',schema);
+
+export default User;
+
